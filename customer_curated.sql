61f84733255b0e48ac3b3f895f102206a82c8696
Vivek Desai <153173803+Vivekdesai25@users.noreply.github.com>
2026-05-21 19:24:53 +0530

    Enhance customer_curated.sql with JSON SerDe settings
    Add ROW FORMAT and SERDEPROPERTIES to customer_curated.sql

--- customer_curated.sql
+++ customer_curated.sql
@@ -10,6 +10,7 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `stedi`.`customer_curated` (
   `phone` string,
   `sharewithfriendsasofdate` bigint
 )
+  
 ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
 WITH SERDEPROPERTIES (
   'ignore.malformed.json' = 'FALSE',
